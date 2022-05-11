8c7fdb43d372fbaffb6116262d89171784a5e121
Cah Bantul <fardan.nozami@gmail.com>
2022-05-11 13:35:18 +0700

    add route url shortener

--- index.js
+++ index.js
@@ -2,23 +2,32 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const dns = require('dns');
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
 
+// json parser
+app.use(express.json());
+
+// urlencoded parser
+app.use(express.urlencoded({ extended: false }));
+
 app.use('/public', express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
+app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
-
-app.listen(port, function() {
+app.get('/api/shorturl', (req, res) => {
+  dns.lookup('example.com', (err, address) => console.log(address));
+});
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
