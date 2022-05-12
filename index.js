065034214c3c017af58bf104749c02514d609f05
Cah Bantul <fardan.nozami@gmail.com>
2022-05-12 13:26:42 +0700

    urlshortener done

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const mongoose = require('mongoose');
+const dns = require('dns');
 
 // import Model Url
 const Url = require('./models/url');
@@ -30,53 +31,56 @@ app.get('/', function (req, res) {
 app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
-app.post('/api/shorturl', async (req, res) => {
-  const urlBody = req.body.url;
-
-  // cek jumlah data dalam database
-  let countUrl = await Url.count();
-  countUrl++;
-
-  // parsing url dari body
-  try {
-    const urlParse = new URL(urlBody);
-
-    // cek sudah ada di databasae atau belum
-    const duplicate = await Url.findOne({ original_url: urlParse.origin });
-    if (duplicate) {
-      res.json({
-        original_url: duplicate.original_url,
-        short_url: duplicate.short_url,
-      });
-    } else {
-      // save to mongoDB
-      Url.create({
-        original_url: urlParse.origin,
-        short_url: countUrl,
-      })
-        .then((result) => {
-          console.log(result);
-          res.json({
-            original_url: result.original_url,
-            short_url: result.short_url,
-          });
-        })
-        .catch((err) => console.log(err));
-    }
-  } catch (error) {
-    res.json({ error: 'invalid url' });
-  }
-});
-
 app.get('/api/shorturl/:shorturl', async (req, res) => {
   Url.findOne({ short_url: req.params.shorturl })
     .then((url) => {
       res.redirect(url.original_url);
     })
     .catch((err) => {
-      res.json({ error: 'salah input url' });
+      res.json({ error: 'invalid url' });
     });
 });
+
+app.post('/api/shorturl', async (req, res) => {
+  const bodyurl = req.body.url;
+
+  // cek jumlah data dalam database
+  let countUrl = await Url.count();
+  countUrl++;
+
+  // url parser
+  const test = new URL(bodyurl);
+
+  // validasi domain name server
+  dns.lookup(test.hostname, (error, address) => {
+    // pengkondisian jika tidak ada alamatnya
+    if (!address) {
+      res.json({ error: 'Invalid URL' });
+    } else {
+      // cek apakah sudah ada di database atau belum
+      Url.findOne({ original_url: bodyurl })
+        .then((result) => {
+          // jika belum ada maka di  ke databse
+          if (!result) {
+            const url = new Url({ original_url: bodyurl, short_url: countUrl });
+            url.save((err, data) => {
+              res.json({
+                original_url: data.original_url,
+                short_url: data.short_url,
+              });
+            });
+          } else {
+            res.json({
+              original_url: result.original_url,
+              short_url: result.short_url,
+            });
+          }
+        })
+        .catch((err) => console.log(err));
+    }
+  });
+});
+
 app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
